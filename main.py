5859e594c6c12c94f0c1209a9696583b4de17140
Elbadry2025 <96208963+Elbadry2025@users.noreply.github.com>
2023-10-23 21:28:35 +0300

    Complete BFS

--- main.py
+++ main.py
@@ -39,42 +39,45 @@ def get_neighbors(state):
 def BFS(initial_state):
     frontier = queue.Queue()
     explored = set()
-    front_set = set()
-    parent_map = {initial_state: initial_state}
+    #front_set = set()
+    parent_map = {initial_state: (initial_state,0)}
     frontier.put(initial_state)
-    front_set.add(initial_state)
+    #front_set.add(initial_state)
     while frontier.qsize() > 0:
         state = frontier.get()
-        front_set.remove(state)
+        #front_set.remove(state)
         explored.add(state)
         if isgoal(state):
-            return parent_map
+            return parent_map,len(explored)
         neighbors = get_neighbors(state)
 
         for i in neighbors:
-            if i not in front_set and i not in explored:
-                parent_map[i] = state
+            if i not in parent_map and i not in explored:
+                parent_map[i] = (state,parent_map[state][1]+1)
                 frontier.put(i)
-                front_set.add(i)
+                #front_set.add(i)
 
     return False
 
 
 grid = "806547231"
-parent = BFS(grid)
+parent,n = BFS(grid)
 
-if parent:
-    x = 0
-    state = "012345678"
-    printGrid(state)
-    while parent[state] != grid:
-        x+=1
-        printGrid(parent[state])
-        state = parent[state]
-    printGrid(grid)
-    print("Steps = ",x)
-else:
-    print("Unsolvable")
+print(n)
+
+# if parent:
+#     x = 0
+#     state = "012345678"
+#     printGrid(state)
+#     while parent[state][0] != grid:
+#         x+=1
+#         printGrid(parent[state][0])
+#         print("depth = ",parent[state][1])
+#         state = parent[state][0]
+#     printGrid(grid)
+#     print("Steps = ",x)
+# else:
+#     print("Unsolvable")
 
 
 
